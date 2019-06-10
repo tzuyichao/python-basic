1be7b71635e745e8c133f9f13cd946af28d5f90d
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-06-10 10:29:32 +0800

    networkx lab1

--- Networkx/erdos_renyi_1.py
+++ Networkx/erdos_renyi_1.py
@@ -1,9 +1,20 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
+
 n = 50
 p = 0.2
 
 g_erdos = nx.erdos_renyi_graph(n, p, seed = 100)
+
+degree = list(g_erdos.degree())
+print(degree)
+print(np.array(degree))
+print(np.array(degree)[:, 1])
+print(max(np.array(degree)[:, 1]))
+edges = len(g_erdos.edges())
+print(f"edges: {edges}")
+
 plt.figure(figsize=(12, 8))
 nx.draw(g_erdos, node_size=10)
 plt.show()
