b05a89792b329124756e8bf95873077a2a9043c7
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-06-13 15:33:19 +0800

    change use solr document api

---
+++ basic/wikipediaapi_2.py
@@ -0,0 +1,20 @@
+import wikipediaapi
+from hanziconv import HanziConv
+import requests
+
+wiki = wikipediaapi.Wikipedia('zh')
+# pages = ['热敏电阻']
+url = "http://localhost:8983/solr/test/update/json/docs"
+pages = ['感測器', '無線感測網路', '自動調溫器', '智能感測器', '胎壓偵測系統', '電流感測器', '穩壓器', '電信號', '热敏电阻', '变频器', '同步電動機', '异步电动机']
+for page in pages:
+    page_content = wiki.page(page)
+    data = {
+        'title': page_content.title,
+        'summary_txt_cjk': HanziConv.toSimplified(page_content.summary),
+        'text_txt_cjk': HanziConv.toSimplified(page_content.text),
+    }
+    #print(data)
+    response = requests.post(url, json=data)
+    print(response.status_code)
+    print(response.text)
+print("Done.")
