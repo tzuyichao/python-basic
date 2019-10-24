58d1406ea2b6227564bfaa71cff883a9f87fdd34
TERENCE.CHAO <terence.chao@deltaww.com>
2019-10-24 13:10:52 +0800

    v1.x introduction on tensorflow v2

--- tensorflow/tensorflow-introduction.py
+++ tensorflow/tensorflow-introduction.py
@@ -1,4 +1,19 @@
-import tensorflow as tf
+# v1.x graph
+import tensorflow.compat.v1 as tf
 import numpy as np
 
+tf.disable_v2_behavior()
 graph = tf.Graph()
+session = tf.InteractiveSession(graph=graph)
+
+x = tf.placeholder(shape=[1, 10], dtype=tf.float32, name='x')
+W = tf.Variable(tf.random_uniform(shape=[10, 5], minval=-0.1, maxval=0.1, dtype=tf.float32), name='w')
+b = tf.Variable(tf.zeros(shape=[5], dtype=tf.float32), name='b')
+h = tf.nn.sigmoid(tf.matmul(x, W) + b)
+
+tf.global_variables_initializer().run()
+
+h_eval = session.run(h, feed_dict={x: np.random.rand(1, 10)})
+print(h_eval)
+
+session.close()
