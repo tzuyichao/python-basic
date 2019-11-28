b3a2594c1dd36c2aa7b8702cc279753a670efb10
TERENCE.CHAO <terence.chao@deltaww.com>
2019-11-28 16:11:34 +0800

    version 0.1

---
+++ Locust/faq-product-api.py
@@ -0,0 +1,17 @@
+from locust import HttpLocust, TaskSet, task
+
+class ProductAPITasks(TaskSet):
+  @task(1)
+  def iabg_zh(self):
+    headers={'content-type': 'application/json'}
+    self.client.post("/product", json={"lang": "zh-tw", "bu": "IABG"}, headers=headers)
+
+  @task(2)
+  def iabg_cn(self):
+    headers={'content-type': 'application/json'}
+    self.client.post("/product", json={"lang": "zh-cn", "bu": "IABG"}, headers=headers)
+
+class FaQTest(HttpLocust):
+  task_set = ProductAPITasks
+  min_wait = 5
+  max_wait = 15
---
+++ Locust/readme.txt
@@ -0,0 +1,3 @@
+Execution:
+=======================
+locust -f faq-product-api.py -H https://textdms.deltaww.com
