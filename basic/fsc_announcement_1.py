a9942808ea3f22a1355f835e4f294e6231a25a95
TERENCE.CHAO <terence.chao@deltaww.com>
2020-01-08 13:57:29 +0800

    fsc partial

--- basic/fsc_announcement_1.py
+++ basic/fsc_announcement_1.py
@@ -1,7 +1,11 @@
 import requests
+import xml.etree.ElementTree as et
 
 url = "https://www.fsc.gov.tw/RSS/Messages?serno=201202290009&language=chinese"
 
 text = requests.get(url).text
 
-print(text)
+xroot = et.fromstring(text)
+
+for item in xroot.findall("./channel/item"):
+    print(item.find("title").text)
