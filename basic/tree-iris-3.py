85da4301f49d8dc5127857bc6a5b63e301911b79
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-13 17:16:14 +0800

    from 23 day exmaple

---
+++ basic/tree-iris-3.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_iris
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+iris = load_iris()
+iris_X = iris.data
+iris_y = iris.target
+
+train_X, test_X, train_y, test_y = train_test_split(iris_X, iris_y, test_size=0.3)
+
+clf = tree.DecisionTreeClassifier()
+iris_clf = clf.fit(train_X, train_y)
+
+test_y_predicted = iris_clf.predict(test_X)
+
+print(test_y_predicted)
+
+print(test_y)
+
+accuracy = metrics.accuracy_score(test_y, test_y_predicted)
+print(accuracy)
