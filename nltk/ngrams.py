59c2a1d1c282022aca94d33b15d601795f68df37
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-17 15:14:04 +0800

    remove punctuation

--- nltk/ngrams.py
+++ nltk/ngrams.py
@@ -1,5 +1,7 @@
 from nltk import ngrams
 from nltk import word_tokenize
+from nltk.corpus import stopwords
+import string
 
 str = 'The cute little boy is playing with the kitten.'
 
@@ -7,4 +9,12 @@ print('source:', str)
 print('Using split()')
 print(list(ngrams(str.split(), 2)))
 print('Using word_tokenize()')
+print('tokenized: ', word_tokenize(str))
+print('stopwords: ', stopwords.words('english'))
+print('string punctuation: ', string.punctuation)
+
+transtab = str.maketrans(string.punctuation, ' '*len(string.punctuation))
+str = str.translate(transtab)
+print('translated source:', str)
+
 print(list(ngrams(word_tokenize(str), 2)))
