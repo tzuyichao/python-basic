a2fec40a66fc29c23c8abaafba0de99dce65b726
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-23 14:57:39 +0800

    index col

--- pandas/read-csv-1.py
+++ pandas/read-csv-1.py
@@ -11,3 +11,10 @@ df = pd.read_csv('../data/goog.csv', parse_dates=['Date'])
 print(df)
 print(df.Date[0])
 print(type(df.Date[0]))
+
+print(df.index)
+
+df = pd.read_csv('../data/goog.csv', parse_dates=['Date'], index_col='Date')
+print(df)
+print(df.index)
+
