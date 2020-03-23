e4c36009e2b1c75bcfd60429d90f80265207740f
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-23 11:24:35 +0800

    dataframe lab 1

---
+++ pandas/dataframe-1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+dates = pd.date_range('2016-04-01', periods=6, freq='D')
+
+print(dates)
+
+temps1 = pd.Series([80, 82, 85, 90, 83, 87], index=dates)
+temps2 = pd.Series([70, 75, 69, 83, 79, 77], index=dates)
+
+tempa_df = pd.DataFrame({'Missoula': temps1, 'Philadephia': temps2})
+
+print(tempa_df)
