9ab642cdd2822cd022813b8ee602b7508ba92548
TzuYi Chao <tzuyichao@mail2000.com.tw>
2020-06-13 22:34:45 +0800

    static method

--- basic/circle.py
+++ basic/circle.py
@@ -1,9 +1,25 @@
 class Circle:
-    def __init__(self):
-        self.radius = 1
+    all_circles = []
+    pi = 3.14159
+
+    def __init__(self, r=1):
+        self.radius = r
+        self.__class__.all_circles.append(self)
+
     def area(self):
-        return self.radius*self.radius*3.14159
+        return self.radius * self.radius * self.__class__.pi
+
+    @staticmethod
+    def total_area():
+        """計算all_circles"""
+        total = 0
+        for c in Circle.all_circles:
+            total = total + c.area()
+        return total
+
 
 if __name__ == "__main__":
     c = Circle()
     print(c.area())
+    c1 = Circle(3)
+    print(Circle.total_area())
