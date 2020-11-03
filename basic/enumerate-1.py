6ba444617dc6ca41c5736ec02f93c7206f40567a
TERENCE.CHAO <terence.chao@deltaww.com>
2020-11-03 14:31:32 +0800

    test

---
+++ basic/enumerate-1.py
@@ -0,0 +1,4 @@
+x = ['a', 'b', 'c']
+list_enum_x = list(enumerate(x))
+
+print(list_enum_x)
---
+++ basic/module-learning-1/mathproj/comp/numeric/n1.py
@@ -0,0 +1,7 @@
+from mathproj import version
+from mathproj.comp import c1
+from mathproj.comp.numeric.n2 import h
+
+def g():
+    print("version is", version)
+    print(h())
