d63364ae9d2bd47831ef501871b5bdecd5213196
TzuYi Chao <tzuyichao0313@gmail.com>
2021-08-27 14:14:11 +0800

    path parameter

--- fastapi/path/main.py
+++ fastapi/path/main.py
@@ -32,3 +32,8 @@ async def get_model(model_name: ModelName):
     if model_name == "lenet":
         return {"model_name": model_name, "message": "LeCNN all the images"}
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@path.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
