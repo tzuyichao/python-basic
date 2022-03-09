e1a98a5a5ee4461c2646acd3fc7d87eda487ba4a
TzuYi Chao <tzuyichao0313@gmail.com>
2022-03-09 14:04:46 +0800

    netcap.py

--- bhp/ch02-netcat.py
+++ bhp/ch02-netcat.py
@@ -10,6 +10,8 @@ class NetCat:
     def __init__(self, args, buffer=None):
         self.args = args
         self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     
     def run(self):
         if self.args.listen:
@@ -19,16 +21,89 @@ class NetCat:
     
     def listen(self):
         print("listen mode")
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+        while True:
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(
+                target=self.handle,
+                args=(client_socket,)
+            )
+            client_thread.start()
+    
+    def handle(self, client_socket):
+        print('in handle()')
+        if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
+        elif self.args.upload:
+            file_buffer = b''
+            while True:
+                data = client_socket.recv(4096)
+                if data:
+                    file_buffer += data
+                else:
+                    break
+            
+            with open(self.args.upload, 'wb') as f:
+                f.write(file_buffer)
+            message = f'Saved filem {self.args.upload}'
+            client_socket.send(message.encode())
+        elif self.args.command:
+            cmd_buffer = b''
+            while True:
+                try:
+                    client_socket.send(b'BHP: #> ')
+                    while '\n' not in cmd_buffer.decode():
+                        cmd_buffer += client_socket.recv(4096)
+                    print(f"execute {cmd_buffer}")
+                    response = execute(cmd_buffer.decode('utf-8'))
+                    print(f"response {response}")
+                    if response:
+                        client_socket.send(response.encode())
+                    cmd_buffer = b''
+                except Exception as e:
+                    print(f'server killed {e}')
+                    self.socket.close()
+                    sys.exit()
     
     def send(self):
         print("send mode")
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
+        
+        try:
+            while True:
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break
+                if response:
+                    print(f'<<< {response}')
+                    try:
+                        buffer = input('> ')
+                        buffer += '\n'
+                        self.socket.send(buffer.encode())
+                    except EOFError as e:
+                        print(f'EOF Error: {e}')
+        except KeyboardInterrupt:
+            print('User terminated.')
+            self.socket.close()
+            sys.exit()
 
 def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
         return
+    print(f'before subprocess {cmd} {shlex.split(cmd)}')
     output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
-    return output.decode()
+    print(f'after subprocess output {output}')
+    return output.decode('utf-8')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -54,6 +129,7 @@ if __name__ == '__main__':
         buffer = ''
     else:
         buffer = sys.stdin.read()
+
     nc = NetCat(args, buffer.encode())
     nc.run()
     
