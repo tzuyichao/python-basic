db54e5d2f12fb7f8f045fc1f1698b4fb600ebea8
TzuYi Chao <tzuyichao0313@gmail.com>
2022-07-14 09:46:00 +0800

    linear search

---
+++ classic/chapter2/dna_search.py
@@ -0,0 +1,34 @@
+from enum import IntEnum
+from typing import Tuple, List
+
+Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
+Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]
+Gene = List[Codon]
+
+gene_str: str = "ACGTGGCTCTCTAACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
+
+def string_to_gene(s: str) -> Gene:
+    gene: Gene = []
+    for i in range(0, len(s), 3):
+        if (i+2) >= len(s):
+            return gene
+        codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i+1]], Nucleotide[s[i+2]])
+        gene.append(codon)
+    return gene
+
+
+my_gene: Gene = string_to_gene(gene_str)
+
+
+def linear_contains(gene: Gene, key_codon: Codon) -> bool:
+    for codon in gene:
+        if codon == key_codon:
+            return True
+    return False
+
+
+acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G)
+gat: Codon = (Nucleotide.G, Nucleotide.A, Nucleotide.T)
+
+print(linear_contains(my_gene, acg))
+print(linear_contains(my_gene, gat))
