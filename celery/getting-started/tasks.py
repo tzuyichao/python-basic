dece4a21f5bbdb48f5b83b8e219651adf95550fc
TzuYi Chao <tzuyichao0313@gmail.com>
2023-05-21 21:14:07 +0800

    lab

--- celery/getting-started/tasks.py
+++ celery/getting-started/tasks.py
@@ -1,7 +1,7 @@
 from celery import Celery
 from celery.utils.log import get_task_logger
 
-app = Celery('tasks', broker='redis://localhost:6379/0', backend="redis://localhost:6379/1")
+app = Celery('tasks', broker='redis://localhost:6379/0', backend="redis://localhost:6379/0")
 
 app.conf.update(
     result_expires=300,
@@ -9,9 +9,10 @@ app.conf.update(
 
 logger = get_task_logger(__name__)
 
-@app.task
+@app.task()
 def add(x, y):
     logger.info('Adding {0} + {1}'.format(x, y))
+    print('Adding {0} + {1}'.format(x, y))
     return x + y
 
 if __name__ == '__main__':
