5a509b02f88033a8c15df68b06b2be131b4a04e4
TzuYi Chao <tzuyichao0313@gmail.com>
2023-05-21 15:11:39 +0800

    getting started

--- celery/getting-started/README.md
+++ celery/getting-started/README.md
@@ -1,11 +1,22 @@
 
 ```bash=
-docker run -d --name activemq -p 5672:5672 -p 61616:61616 -p 8161:8161 webcenter/activemq
+docker run -d -p 5672:5672 rabbitmq
+
+docker run -d -p 6379:6379 redis
 
 celery -A tasks worker --loglevel=INFO
+
+celery -A tasks worker -E --loglevel=INFO
 ```
 
 ```bash=
+from tasks import add
+add.run(3, 3)
 
+r = add.apply_async((10, 10), expires=1)
+
+r.ready()
+
+r.get(timeout=10)
 ```
 
--- celery/getting-started/tasks.py
+++ celery/getting-started/tasks.py
@@ -1,7 +1,18 @@
 from celery import Celery
+from celery.utils.log import get_task_logger
 
-app = Celery('task', broker='pyamqp://admin:admin@localhost')
+app = Celery('tasks', broker='pyamqp://guest@localhost//', backend="redis://localhost:6379/0")
+
+app.conf.update(
+    result_expires=300,
+)
+
+logger = get_task_logger(__name__)
 
 @app.task
 def add(x, y):
+    logger.info('Adding {0} + {1}'.format(x, y))
     return x + y
+
+if __name__ == '__main__':
+    app.start(["worker"])
