cf6cb1853b258958ad22d91104c23e1063723979
TzuYi Chao <tzuyichao0313@gmail.com>
2024-04-03 17:16:19 +0800

    Update time_difflib_to_memory.py

--- basic/time_difflib_to_memory.py
+++ basic/time_difflib_to_memory.py
@@ -9,22 +9,23 @@ def diff(lh, rh) -> str:
     result = '\n'.join(diff)
     return result
 
-def parse(result: str) -> tuple:
+def parse(result: str, pk_columns: set) -> tuple:
+    pk_len = len(pk_columns)
     diff_output = result.strip().split("\n")[3:]
     unique_in_before = set()
     unique_in_after = set()
     differences = set()
 
-    observed_pks = {}
+    observed_pks = dict()
 
     for line in diff_output:
         if line.startswith('@@'):
             continue
         if line.startswith('-') or line.startswith('+'):
             action = line[0]
-            pk = line[1:].split(',')[0]
+            pk = ','.join(line[1:].split(',')[0:pk_len])
             
-            if pk in observed_pks and observed_pks[pk] != action:
+            if pk in observed_pks.keys() and observed_pks[pk] != action:
                 differences.add(pk)
                 unique_in_before.discard(pk)
                 unique_in_after.discard(pk)
@@ -37,7 +38,7 @@ def parse(result: str) -> tuple:
                     if pk not in differences:
                         unique_in_after.add(pk)
         else:
-            pk = line.split(',')[0]
+            pk = ','.join(line.split(',')[0:pk_len])
 
     print("Unique in before.py:", unique_in_before)
     print("Unique in after.py:", unique_in_after)
@@ -49,13 +50,15 @@ if __name__ == '__main__':
     engineMDMCDC = create_engine(f"mssql+pymssql://datagov:datagov@localhost/MDM_CDC?charset=utf8")
     schema = 'dbo'
     tableName = 'MDM_CUSTOMER_MASTER'
-    lh = get_data(engine=engineMDM, schema=schema, tableName=tableName).splitlines()
-    rh = get_data(engine=engineMDMCDC, schema=schema, tableName=tableName).splitlines()
+    lh_data, pk_columns = get_data(engine=engineMDM, schema=schema, tableName=tableName)
+    rh_data, _ = get_data(engine=engineMDMCDC, schema=schema, tableName=tableName)
+    lh = lh_data.splitlines()
+    rh = rh_data.splitlines()
     execution_time = timeit.timeit(lambda: diff(lh, rh), number=1)
     print()
     print(f"Function diff {tableName} executed in {execution_time} seconds.")
     res = diff(lh, rh)
     sys.stdout.writelines(res)
     print()
-    unique_in_source, unique_in_sink, differences = parse(res)
+    unique_in_source, unique_in_sink, differences = parse(res, pk_columns)
 
